44b072892c05e9c3f9c75c90f9afd8c15ec96da4
danielrutilo1997 <85973762+danielrutilo1997@users.noreply.github.com>
2025-12-25 18:51:26 -0800

    finished cybersecurityevent class

--- lib/models/cyber_security_event.dart
+++ lib/models/cyber_security_event.dart
@@ -1,23 +1,38 @@
 class CyberSecurityEvent {
+  // Fields
   final String title;
   final String description;
   final String url;
   final DateTime publishedDate;
-  final String summary;
+  final String? summary;
 
   // Constructor
-  CyberSecurityEvent(
-    this.title,
-    this.description,
-    this.url,
-    this.publishedDate,
+  CyberSecurityEvent({
+    required this.title,
+    required this.description,
+    required this.url,
+    required this.publishedDate,
     this.summary,
-  );
+  });
 
   //jason processor
   factory CyberSecurityEvent.fromJson(Map<String, dynamic> json) {
-    //Call api '
-    //Create CSEvent
-    //return CSEvent
+    return CyberSecurityEvent(
+      title: json['title'],
+      description: json['description'],
+      url: json['url'],
+      publishedDate: DateTime.parse(json['publishedDate']),
+      summary: json['summary'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'description': description,
+      'url': url,
+      'publishedDate': publishedDate.toIso8601String(),
+      'summary': summary,
+    };
   }
 }
