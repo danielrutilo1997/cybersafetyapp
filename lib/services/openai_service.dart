a72935891cfe7b9b043985b2c49ece76bc5aea00
danielrutilo1997 <85973762+danielrutilo1997@users.noreply.github.com>
2026-01-02 21:29:37 -0800

    werkin on openaiservice

--- lib/services/openai_service.dart
+++ lib/services/openai_service.dart
@@ -1,12 +1,30 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'dart:io';
 
 class OpenAIService {
+  String? apiKey;
+  static const String apiUrl = 'https://api.openai.com/v1/chat/completions';
+  Future<String> loadAPIKey() async {
+    try {
+      // 1. We're reading the file from keys.json
+      final file = File('keys.json');
+      final contents = await file.readAsString();
 
-  Future<String> summarizeArticle(String articleText){
+      // 2. Parse JSON
+      final json = jsonDecode(contents);
 
+      // 3. Extract API key
+      apiKey = json['OPEN_AI_API_KEY'];
 
-    return [];
+      return apiKey;
+    } catch (e) {
+      print('Error loading API key: $e');
+      throw Exception('Failed to load API key');
+    }
   }
 
+  Future<String> summarizeArticle(String articleText) {
+    return [];
+  }
 }
