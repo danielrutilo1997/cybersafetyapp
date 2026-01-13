221ece028bf705a7e14081586884b0b82350c689
danielrutilo1997 <daniel.rutilo@gmail.com>
2026-01-13 15:20:16 -0800

    commit

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,27 @@
 import 'package:cybersafetyapp/models/cyber_security_event.dart';
-import 'package:cybersafetyapp/services/news_service.dart'
-import 'package:cybersafetyapp/services/openai_service.dart'
+import 'package:cybersafetyapp/services/news_service.dart';
+import 'package:cybersafetyapp/services/openai_service.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget{
+  const HomeScreen({Key? key}) : super(key: key);
   
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // State variables
+  @override
+  void initState() {
+    super.initState();
+    // Initialize and fetch data when screen loads
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // Return your UI here (Scaffold with AppBar, body with ListView, etc.)
+    
+  }
 }
 
